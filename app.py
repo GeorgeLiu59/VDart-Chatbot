1ac2dd48d5a8e5c38f129add88e987a4fda97f69
GeorgeLiu59 <77650520+GeorgeLiu59@users.noreply.github.com>
2024-04-10 11:11:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,26 @@ import sys
 from dotenv import load_dotenv
 import streamlit as st
 import pandas as pd
-from pandasai import SmartDataframe
+from pandasai import Agent
 from pandasai.llm import OpenAI
+import matplotlib
 
 load_dotenv()
 
-llm = OpenAI(api_token='ADD OPENAI API KEY HERE')
+matplotlib.use(backend="TkAgg")
+
+#Input OpenAI API key here
+llm = OpenAI(api_token='PUT OPENAI KEY HERE')
 
 st.set_page_config(layout="wide")
 
 st.title("VDart Chatbot")
 
-df = pd.read_csv("all-contracts-filtered.csv", encoding='utf-8', engine='python')
+df = pd.read_csv("sample-data-set-inactives.csv", encoding='utf-8', engine='python')
 
 df = df.dropna(axis=1, how="all")
 
 col1, col2 = st.columns(2)
-
-# uploaded_file = st.file_uploader("Upload CSV For Analysis", type="csv")
 
 with col1:
     st.write(df)
@@ -32,11 +34,12 @@ with col2:
 
     if generate:
         if prompt:
-            #st.write("OpenAI is generating an answer, please wait...")
             with st.spinner("Generating answer..."):
-                #st.write(pandas_ai.run(df, prompt=prompt))
-                smartDf = SmartDataframe(df, config={"llm": llm})
-                st.write(smartDf.chat(prompt))
+                agent = Agent(df, config={"llm": llm, "verbose": True})
+                response = agent.chat(prompt)
+                st.write(response)
+                st.write(agent.explain())
             
         else:
             st.warning("Please upload a csv file and enter your prompt")
+            
